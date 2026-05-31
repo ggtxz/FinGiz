6c12ee18420b3974650f1b81f33f912c5296200d
Gimenez <ggimenezt@gmail.com>
2026-05-31 19:21:19 -0300

    Iniciando repo

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Gustavo/fingiz
+# Build directory: C:/Users/Gustavo/fingiz/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
