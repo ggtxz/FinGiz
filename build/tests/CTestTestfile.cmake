6bbf2da62d5e6d662bd94dddbb70527b0a927af6
Gimenez <ggimenezt@gmail.com>
2026-06-05 19:33:18 -0300

    Remove build directory from repository

--- build/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Gustavo/fingiz/tests
-# Build directory: C:/Users/Gustavo/fingiz/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/Gustavo/fingiz/build/tests/account[1]_include.cmake")
-add_test([=[account]=] "C:/Users/Gustavo/fingiz/build/tests/account.exe")
-set_tests_properties([=[account]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Gustavo/fingiz/tests/CMakeLists.txt;19;add_test;C:/Users/Gustavo/fingiz/tests/CMakeLists.txt;0;")
